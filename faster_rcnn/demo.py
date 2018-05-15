772bcef020a9cdcfaf764dd313e32376667828ca
TeppeiKanayama <1994teppei0615kanayama@gmail.com>
2018-05-15 12:52:58 +0900

    personのみ検出+しきい値0.05

--- faster_rcnn/demo.py
+++ faster_rcnn/demo.py
@@ -4,15 +4,24 @@ import matplotlib.pyplot as plot
 import chainer
 
 from chainercv.datasets import voc_bbox_label_names
-from chainercv.links import FasterRCNNVGG16
-from chainercv import utils
-from chainercv.visualizations import vis_bbox
+#from chainercv.links import FasterRCNNVGG16
+import sys
+sys.path.append("/home/mil/kanayama/workspace/chainercv/chainercv/links/model/faster_rcnn")
+from faster_rcnn_vgg import FasterRCNNVGG16
 
+from chainercv import utils
+#from chainercv.visualizations import vis_bbox
+sys.path.append("/home/mil/kanayama/workspace/chainercv/chainercv/visualizations")
+from vis_bbox import vis_bbox
+import cupy
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('--pretrained_model', default='voc07')
+    parser.add_argument('--root_path', default="/data/unagi0/food/train_set/")
+    parser.add_argument('--image_list', default="/data/unagi0/food/tmp/train1000.txt")
+    parser.add_argument('--dst_path', default="/data/unagi0/food/tmp/dst/")
     parser.add_argument('image')
     args = parser.parse_args()
 
@@ -24,15 +33,24 @@ def main():
         chainer.cuda.get_device_from_id(args.gpu).use()
         model.to_gpu()
 
-    img = utils.read_image(args.image, color=True)
-    bboxes, labels, scores = model.predict([img])
-    bbox, label, score = bboxes[0], labels[0], scores[0]
+    image_list = open(args.image_list, "r")
+    for filename in image_list:
+        filename = filename.split('\n')[0]
+        print(filename)
+        #img = utils.read_image(args.root_path + filename, color=True)
+        img = utils.read_image(args.image, color=True)
+        import pdb; pdb.set_trace()
+        bboxes, labels, scores = model.predict([img])
+        bbox, label, score = bboxes[0], labels[0], scores[0]
 
-    vis_bbox(
-        img, bbox, label, score, label_names=voc_bbox_label_names)
-    plot.show()
-    plot.savefig("dst.jpg")
+        vis_bbox(
+            img, bbox, label, score, label_names=voc_bbox_label_names)
+        #plot.show()
+        plot.savefig(args.dst_path + args.image.split("/")[-1])
+        break
+        #plot.savefig(args.dst_path + filename)
 
+    image_list.close()
 
 if __name__ == '__main__':
     main()
